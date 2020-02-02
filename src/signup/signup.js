282eefd3e5d658d72b4e3319b3f2c430c2019373
Patrick Tunezerwane <tunezepatrick@gmail.com>
2020-02-02 17:38:21 +0200

    create components

---
+++ src/signup/signup.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Signup extends Component {
+  state = {  }
+  render() { 
+    return ( <h1>this is Signup</h1> );
+  }
+}
+ 
+export default Signup;
