80b588679a1827b6b481b0f244db7bdb51747f4f
Patrick Tunezerwane <tunezepatrick@gmail.com>
2020-02-02 19:15:16 +0200

    change extantion

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Paper from '@material-ui/core/Paper';
+import withStyles from '@material-ui/core/styles/withStyles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import styles from './signupStyles';
+import firebase from 'firebase';
+
 class Signup extends Component {
   state = {  }
   render() { 
-    return ( <h1>this is Signup</h1> );
+    const { classes } = this.props;
+    return (
+      <main className={classes.main}>
+        <CssBaseline></CssBaseline>
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+
+          <form onSubmit={(event) => this.submitSignUp(event)} className={classes.form}>
+            <FormControl>
+              <InputLabel required fullwidth margin="normal">Your email</InputLabel>
+              <Input></Input>
+            </FormControl>
+          </form>
+        </Paper>
+      </main>
+    );
+  }
+
+  submitSignUp = (event) => {
+
   }
 }
  
-export default Signup;
+export default withStyles(styles)(Signup);
