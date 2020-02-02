11a0a030a0a6d79eb0bab22e52878f7a3514de98
Patrick Tunezerwane <tunezepatrick@gmail.com>
2020-02-02 16:43:53 +0200

    set up firebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+import 'firebase/firestore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+firebase.initializeApp({
+  apiKey: "AIzaSyAaNlJ3Xy90qWb3cHC6WinTZN1a6T-vNvI",
+  authDomain: "we-talk-36685.firebaseapp.com",
+  databaseURL: "https://we-talk-36685.firebaseio.com",
+  projectId: "we-talk-36685",
+  storageBucket: "we-talk-36685.appspot.com",
+  messagingSenderId: "1071788481604",
+  appId: "1:1071788481604:web:3fc8c7b1edc0646b337011",
+  measurementId: "G-226CH8RWWE"
+})
+
+ReactDOM.render(<div>hello</div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
